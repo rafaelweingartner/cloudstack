22750553c8760e6e1c37adac75957d1e1bd491a1
Rodrigo Lopez <rodrigo_duarte.l@hotmail.com>
2020-07-15 09:36:55 -0300

    update deprecated mockito

--- usage/src/test/java/com/cloud/usage/UsageManagerImplTest.java
+++ usage/src/test/java/com/cloud/usage/UsageManagerImplTest.java
@@ -26,7 +26,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.cloud.event.EventTypes;
 import com.cloud.event.UsageEventVO;
